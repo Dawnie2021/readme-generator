e6e595aab77ba819d86618605caf6a0655f613e0
Dawn nguyen <dawnnguyen2021@gmail.com>
2023-12-15 00:50:42 -0500

    created an array of questions for user input

--- utils/index.js
+++ utils/index.js
@@ -4,8 +4,50 @@ const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Created an array of questions for user input
+const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the name of your project?'
+},
+{
+    type: 'input',
+    name: "description",
+    message: 'What is the purpose of this project?'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'What technologies did you use for this project?'
+},
+{
+    type: 'checkbox',
+    name: 'license',
+    message: 'What license is applicable to this project?',
+    choices: []
+},
+{
+    type: 'input',
+    name: 'contributers',
+    message: 'Please list any contributers on this project.'
+},
+{
+    type: 'input',
+    name: 'test',
+    message: 'Please provide information on any tests that you may have encountered'
+}
+
+
+
+
+
+
+
+
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
